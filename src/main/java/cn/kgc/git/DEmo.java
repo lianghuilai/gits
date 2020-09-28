526d64c3dfcf47363708f68e164c30b6758fdffd
qwe <245451789@qq.com>
2020-09-28 15:17:07 +0800

    git2

--- src/main/java/cn/kgc/git/DEmo.java
+++ src/main/java/cn/kgc/git/DEmo.java
@@ -5,6 +5,7 @@ public class DEmo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("11111111111111");
+        System.out.println("2222222222222");
 	}
 
 }
